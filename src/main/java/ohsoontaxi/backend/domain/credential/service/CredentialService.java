5489d757dd709fcc8c9593cc5ea45a4a3d34c542
ihun-il <hunil9978@gmail.com>
2023-04-20 23:35:24 +0900

    :art:: Login 수정 (#11)

--- src/main/java/ohsoontaxi/backend/domain/credential/service/CredentialService.java
+++ src/main/java/ohsoontaxi/backend/domain/credential/service/CredentialService.java
@@ -2,6 +2,7 @@ package ohsoontaxi.backend.domain.credential.service;
 
 import lombok.RequiredArgsConstructor;
 import ohsoontaxi.backend.domain.credential.presentation.dto.request.LoginRequestDto;
+import ohsoontaxi.backend.domain.credential.presentation.dto.response.AccessTokenDto;
 import ohsoontaxi.backend.domain.user.domain.User;
 import ohsoontaxi.backend.global.security.JwtTokenProvider;
 import ohsoontaxi.backend.global.utils.user.UserUtils;
@@ -17,10 +18,10 @@ public class CredentialService {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
-    public String login(Long userId){
+    public AccessTokenDto login(Long userId){
         User user = userUtils.getUserById(userId);
         String accessToken = jwtTokenProvider.generateAccessToken(userId, user.getAccountRole());
-        return accessToken;
+        return new AccessTokenDto(accessToken);
     }
 
 }
