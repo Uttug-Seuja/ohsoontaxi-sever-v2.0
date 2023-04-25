52af430a7dd841a974a1eee023ea07818ab0aaca
chanwoo <mdsoo55828@gmail.com>
2023-04-25 21:19:55 +0900

    :page_facing_up:: ReserveRepositoryCustom 생성 (#26)

--- src/main/java/ohsoontaxi/backend/domain/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/ohsoontaxi/backend/domain/reservation/domain/repository/ReservationRepository.java
@@ -5,13 +5,30 @@ import ohsoontaxi.backend.domain.reservation.domain.Reservation;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long>,ReservationRepositoryCustom {
     @Override
     Optional<Reservation> findById(Long id);
     Slice<Reservation> findSliceBy(Pageable pageable);
 
-    Slice<Reservation> findAllByUserId(Pageable pageable,Long userId);
+    //Slice<Reservation> findAllByUserId(Pageable pageable,Long userId);
+    //List<Reservation> findAllByUserIdOrderByCreatedDate(Long userId);
+
+    List<Reservation> findTop5ByOrderByIdDesc();
+
+    @Query("select distinct r from Reservation r"+
+            " join fetch r.participations p"+
+            " where p.user.id = :userId order by r.createdDate")
+    List<Reservation> findParticipatedReservation(@Param("userId") Long userId);
+
+    @Query("select distinct r from Reservation r"+
+            " where r.user.id = :userId order by r.createdDate")
+    List<Reservation> findReservedByMe(@Param("userId") Long userId);
+
+
 }
---
+++ src/main/java/ohsoontaxi/backend/domain/reservation/domain/repository/ReservationRepositoryImpl.java
@@ -0,0 +1,85 @@
+package ohsoontaxi.backend.domain.reservation.domain.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import ohsoontaxi.backend.domain.reservation.domain.QReservation;
+import ohsoontaxi.backend.domain.reservation.domain.Reservation;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import java.util.List;
+
+import static io.micrometer.common.util.StringUtils.isEmpty;
+import static ohsoontaxi.backend.domain.reservation.domain.QReservation.*;
+
+@RequiredArgsConstructor
+public class ReservationRepositoryImpl implements ReservationRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Slice<Reservation> keywordBySlice(String keyword,Pageable pageable) {
+        List<Reservation> reservations = queryFactory
+                .selectDistinct(reservation)
+                .from(reservation)
+                .where(
+                        titleContain(keyword)
+                )
+                .orderBy(reservation.title.length().asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        return new SliceImpl<>(reservations, pageable, hasNext(reservations,pageable));
+    }
+
+    @Override
+    public Slice<Reservation> searchBySlice(String keyword, Pageable pageable) {
+
+        List<Reservation> reservations = queryFactory.selectFrom(reservation)
+                .where(
+                        // 기타 조건들
+                        titleContain(keyword).
+                                or(startContain(keyword)).
+                                or(destinationContain(keyword))
+                )
+                .orderBy(reservation.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize()+1)
+                .fetch();
+
+        // 무한 스크롤 처리
+        return new SliceImpl<>(reservations, pageable, hasNext(reservations,pageable));
+    }
+
+
+
+
+    // 무한 스크롤 방식 처리하는 메서드
+    private boolean hasNext(List<Reservation> results,Pageable pageable) {
+
+        boolean hasNext = false;
+
+        // 조회한 결과 개수가 요청한 페이지 사이즈보다 크면 뒤에 더 있음, next = true
+        if (results.size() > pageable.getPageSize()) {
+            results.remove(pageable.getPageSize());
+            hasNext = true;
+        }
+
+        return hasNext;
+    }
+
+    private BooleanExpression titleContain(String keyword) {
+        return isEmpty(keyword) ? null : reservation.title.contains(keyword);
+    }
+
+    private BooleanExpression startContain(String keyword) {
+        return isEmpty(keyword) ? null : reservation.startPoint.contains(keyword);
+    }
+
+    private BooleanExpression destinationContain(String keyword) {
+        return isEmpty(keyword) ? null : reservation.destination.contains(keyword);
+    }
+}
