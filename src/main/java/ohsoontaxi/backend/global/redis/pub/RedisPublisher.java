fff8856a70b321deacaceaad5b0640753d2cad79
chanwoo <mdsoo55828@gmail.com>
2023-08-27 13:34:13 +0900

    :page_facing_up:: redis pub/sub (#61)

---
+++ src/main/java/ohsoontaxi/backend/global/redis/pub/RedisPublisher.java
@@ -0,0 +1,27 @@
+package ohsoontaxi.backend.global.redis.pub;
+
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ohsoontaxi.backend.domain.chat.presentation.dto.request.ChatMessageSaveDto;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class RedisPublisher {
+
+    /*
+     * redis 발행 서비스
+     *
+     * */
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessageSaveDto messageDto) {
+
+        redisTemplate.convertAndSend(topic.getTopic(), messageDto);
+    }
+}
