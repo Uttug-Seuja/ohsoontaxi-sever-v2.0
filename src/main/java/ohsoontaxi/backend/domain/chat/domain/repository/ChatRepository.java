980a688f55b79d0f3e2419194842dafa777c9d07
chanwoo <mdsoo55828@gmail.com>
2023-08-29 16:50:08 +0900

    :sparkles:: ChatRepository reservation null 및 2일전 채팅 가져오기 기능 추가(#77)

--- src/main/java/ohsoontaxi/backend/domain/chat/domain/repository/ChatRepository.java
+++ src/main/java/ohsoontaxi/backend/domain/chat/domain/repository/ChatRepository.java
@@ -6,6 +6,9 @@ import ohsoontaxi.backend.domain.chat.domain.Chat;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +17,10 @@ public interface ChatRepository extends JpaRepository<Chat,Long> {
     Slice<Chat> findAllByCreatedAtBeforeAndReservationIdOrderByCreatedAtDesc(String cursorCreatedAt, Long reservationId, Pageable pageable);
 
     List<Chat> findAllByCreatedAtAfterOrderByCreatedAtDesc(String cursorCreatedAt);
+
+    List<Chat> findAllByReservationIsNotNullAndCreatedAtAfterOrderByCreatedAtDesc(String cursorCreatedAt);
+
+    @Modifying(flushAutomatically = true)
+    @Query("update Chat c set c.reservation = null where c.reservation.id = :reservationId")
+    void updateReservationNull(@Param("reservationId") Long reservationId);
 }
