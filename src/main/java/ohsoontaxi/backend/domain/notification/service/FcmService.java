22a270f7f40e1b34cbc14dfd02647ff7d769a0ca
ihun-il <hunil9978@gmail.com>
2023-08-29 22:35:10 +0900

    :page_facing_up:: 비동기로 그룹 메세지 보내기 로직

---
+++ src/main/java/ohsoontaxi/backend/domain/notification/service/FcmService.java
@@ -0,0 +1,35 @@
+package ohsoontaxi.backend.domain.notification.service;
+
+import com.google.api.core.ApiFuture;
+import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class FcmService {
+
+    public ApiFuture<BatchResponse> sendGroupMessageAsync(
+            List<String> tokenList, String title, String content) {
+        MulticastMessage multicast =
+                MulticastMessage.builder()
+                        .addAllTokens(tokenList)
+                        .setNotification(
+                                Notification.builder()
+                                        .setTitle(title)
+                                        .setBody(content)
+                                        .build())
+                        .setApnsConfig(
+                                ApnsConfig.builder()
+                                        .setAps(Aps.builder().setSound("default").build())
+                                        .build())
+                        .build();
+
+        return FirebaseMessaging.getInstance().sendMulticastAsync(multicast);
+    }
+
+}
