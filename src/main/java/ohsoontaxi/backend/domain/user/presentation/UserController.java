5f5f4ca7c77b70c4b36d7690a7f50021938d18c2
chanwoo <mdsoo55828@gmail.com>
2023-09-13 16:29:03 +0900

    :page_facing_up:: UserController 공백 제거

--- src/main/java/ohsoontaxi/backend/domain/user/presentation/UserController.java
+++ src/main/java/ohsoontaxi/backend/domain/user/presentation/UserController.java
@@ -3,15 +3,11 @@ package ohsoontaxi.backend.domain.user.presentation;
 import jakarta.persistence.EntityManager;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import ohsoontaxi.backend.domain.asset.domain.ProfileImage;
-import ohsoontaxi.backend.domain.temperature.domain.Temperature;
-import ohsoontaxi.backend.domain.user.domain.User;
 import ohsoontaxi.backend.domain.user.presentation.dto.request.ChangeProfileRequest;
 import ohsoontaxi.backend.domain.user.presentation.dto.response.DeployTestDto;
 import ohsoontaxi.backend.domain.user.presentation.dto.response.UserProfileResponse;
 import ohsoontaxi.backend.domain.user.service.UserService;
-import ohsoontaxi.backend.global.common.user.Gender;
-import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-    private final EntityManager em;
 
     @PatchMapping("/profile")
     public UserProfileResponse changeProfilePath(@RequestBody @Valid ChangeProfileRequest changeProfileRequest){
