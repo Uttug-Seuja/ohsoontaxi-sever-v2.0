829a67a9bc37a2c50b4e8c356350c5266b6f305d
chanwoo <mdsoo55828@gmail.com>
2023-09-13 16:02:11 +0900

    :page_facing_up:: SwaggerConfig 추가 및 build.gradle 추가

---
+++ src/main/java/ohsoontaxi/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package ohsoontaxi.backend.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(swaggerInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info swaggerInfo() {
+        License license = new License();
+        license.setUrl("https://github.com/Uttug-Seuja/ohsoontaxi-sever-v2.0");
+        license.setName("ohsoontaxi");
+
+        return new Info()
+                .version("v0.0.1")
+                .title("\"ohsoontaxi 서버 API문서\"")
+                .description("ohsoontaxi 서버의 API 문서 입니다.")
+                .license(license);
+    }
+
+    private static List<String> getTags(HandlerMethod handlerMethod) {
+        List<String> tags = new ArrayList<>();
+
+        Tag[] methodTags = handlerMethod.getMethod().getAnnotationsByType(Tag.class);
+        List<String> methodTagStrings =
+                Arrays.stream(methodTags).map(Tag::name).collect(Collectors.toList());
+
+        Tag[] classTags = handlerMethod.getClass().getAnnotationsByType(Tag.class);
+        List<String> classTagStrings =
+                Arrays.stream(classTags).map(Tag::name).collect(Collectors.toList());
+        tags.addAll(methodTagStrings);
+        tags.addAll(classTagStrings);
+        return tags;
+    }
+}
