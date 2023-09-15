9df05dc2c08760dd482f41f718a26832859ad575
ihun-il <hunil9978@gmail.com>
2023-09-15 21:23:24 +0900

    :bug:: EmailMessage(email, Code) NotEmpty 처리

--- src/main/java/ohsoontaxi/backend/domain/email/presentation/dto/request/CodeRequestDto.java
+++ src/main/java/ohsoontaxi/backend/domain/email/presentation/dto/request/CodeRequestDto.java
@@ -1,10 +1,15 @@
 package ohsoontaxi.backend.domain.email.presentation.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CodeRequestDto {
 
+    @NotNull
     private String email;
+
+    @NotEmpty
     private String code;
 }
