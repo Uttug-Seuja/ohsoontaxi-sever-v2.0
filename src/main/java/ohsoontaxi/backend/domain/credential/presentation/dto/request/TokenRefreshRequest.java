a4c29a16189b8ab02144397c008c1fb1cbfa5f74
ihun-il <hunil9978@gmail.com>
2023-09-15 23:22:08 +0900

    :bug:: Credential(name, gender, accessToken) NotEmpty 처리

--- src/main/java/ohsoontaxi/backend/domain/credential/presentation/dto/request/TokenRefreshRequest.java
+++ src/main/java/ohsoontaxi/backend/domain/credential/presentation/dto/request/TokenRefreshRequest.java
@@ -1,8 +1,11 @@
 package ohsoontaxi.backend.domain.credential.presentation.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class TokenRefreshRequest {
+
+    @NotNull
     private String refreshToken;
 }
