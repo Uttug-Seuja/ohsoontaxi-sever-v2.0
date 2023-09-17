0fac5ef327fe09eb26de57113b2e51edeec00847
ihun-il <hunil9978@gmail.com>
2023-09-17 23:20:37 +0900

    :zap:: EmailController swagger 작성

--- src/main/java/ohsoontaxi/backend/domain/email/presentation/EmailController.java
+++ src/main/java/ohsoontaxi/backend/domain/email/presentation/EmailController.java
@@ -1,5 +1,9 @@
 package ohsoontaxi.backend.domain.email.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import ohsoontaxi.backend.domain.email.presentation.dto.request.CodeRequestDto;
 import ohsoontaxi.backend.domain.email.presentation.dto.request.EmailRequestDto;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Email 인증", description = "Email 인증 관련 API")
 @RequestMapping("/api/v1/email")
 @RestController
 @RequiredArgsConstructor
@@ -16,15 +21,17 @@ public class EmailController {
 
     private final EmailService emailService;
 
-    // 회원가입 이메일 인증
+    @SecurityRequirements
+    @Operation(summary = "학교 Email로 인증 코드 보내기")
     @PostMapping
-    public void sendJoinMail(@RequestBody EmailRequestDto emailRequestDto) {
+    public void sendJoinMail(@Valid @RequestBody EmailRequestDto emailRequestDto) {
         emailService.sendMail(emailRequestDto, "email");
     }
 
-    //코드가 맞는지 확인
+    @SecurityRequirements
+    @Operation(summary = "인증 코드 맞는지 확인")
     @PostMapping("/code")
-    public void checkCode(@RequestBody CodeRequestDto codeRequestDto) {
+    public void checkCode(@Valid @RequestBody CodeRequestDto codeRequestDto) {
         emailService.checkCodeCorrect(codeRequestDto);
     }
 
