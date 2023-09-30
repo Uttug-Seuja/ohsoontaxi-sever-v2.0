5ab283d269661d6c6f0b1394bfaea3bb0e5d621f
ihun-il <hunil9978@gmail.com>
2023-09-30 18:07:41 +0900

    :page_facing_up:: swagger ImageController 설명 추가

--- src/main/java/ohsoontaxi/backend/domain/image/presentation/ImageController.java
+++ src/main/java/ohsoontaxi/backend/domain/image/presentation/ImageController.java
@@ -1,5 +1,8 @@
 package ohsoontaxi.backend.domain.image.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import ohsoontaxi.backend.domain.image.presentation.dto.UploadImageResponse;
 import ohsoontaxi.backend.domain.image.service.ImageService;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "업로드", description = "업로드 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/images")
 @RestController
@@ -16,8 +20,13 @@ public class ImageController {
 
     private final ImageService imageService;
 
+    @Operation(summary = "사진 업로드")
     @PostMapping("/upload")
-    public UploadImageResponse uploadImage(@RequestPart MultipartFile file) {
+    public UploadImageResponse uploadImage(
+            @Parameter(name = "file",
+                    description = "multipart/form-data 형식의 이미지 리스트를 input으로 받습니다. 이때 key 값은 file 입니다.",
+                    required = true)
+            @RequestPart MultipartFile file) {
         return imageService.uploadImage(file);
     }
 }
