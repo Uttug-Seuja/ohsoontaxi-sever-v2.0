7c6b913241547441be51857518b2b1756b15a133
chanwoo <mdsoo55828@gmail.com>
2023-10-11 16:40:12 +0900

    :page_facing_up:: StompHandler SUBSCRIBE,CONNECT 수정

--- src/main/java/ohsoontaxi/backend/global/config/StompHandler.java
+++ src/main/java/ohsoontaxi/backend/global/config/StompHandler.java
@@ -4,15 +4,11 @@ package ohsoontaxi.backend.global.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import ohsoontaxi.backend.domain.chat.presentation.dto.request.ChatMessageSaveDto;
-import ohsoontaxi.backend.domain.chat.service.ChatRoomService;
 import ohsoontaxi.backend.domain.participation.domain.Participation;
 import ohsoontaxi.backend.domain.participation.service.ParticipationUtils;
 import ohsoontaxi.backend.global.property.JwtProperties;
-import ohsoontaxi.backend.global.redis.pub.RedisPublisher;
 import ohsoontaxi.backend.global.security.JwtTokenProvider;
 import ohsoontaxi.backend.global.utils.chat.ChatUtils;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -26,18 +22,12 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class StompHandler implements ChannelInterceptor {
-    public static final String TOKEN = "token";
+    //public static final String TOKEN = "token";
     public static final String SIMP_DESTINATION = "simpDestination";
     public static final String SIMP_SESSION_ID = "simpSessionId";
     public static final String INVALID_ROOM_ID = "InvalidRoomId";
 
     private final ChatUtils chatUtils;
-
-    private final ChannelTopic topic;
-
-    private final ChatRoomService chatRoomService;
-
-    private final RedisPublisher redisPublisher;
 
     private final ParticipationUtils participationUtils;
 
@@ -53,91 +43,42 @@ public class StompHandler implements ChannelInterceptor {
 
         if (StompCommand.CONNECT == accessor.getCommand()) {
 
-//            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
-//
-//            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
-//
-//            log.info("token={}",token);
-//
-//            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
-
-
-            // TODO: 2023/08/14 웹소켓 테스트
-
             String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
 
-            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
-
-            log.info("token={}",token);
+            jwtTokenProvider.resolveTokenWeb(rawHeader);
 
             jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
-            String userId = jwtTokenProvider.getUserId(token);
-
-            //String roomId = accessor.getFirstNativeHeader("roomId");
-
-            log.info("userId={}",userId);
-
-
-            String destination = accessor.getFirstNativeHeader(SIMP_DESTINATION);
-
-            String sessionId = accessor.getFirstNativeHeader(SIMP_SESSION_ID);
-
-//            String destination = Optional.ofNullable(
-//                    (String) message.getHeaders().get(SIMP_DESTINATION)
-//            ).orElse(INVALID_ROOM_ID);
-
-//            String sessionId = Optional.ofNullable(
-//                    (String) message.getHeaders().get(SIMP_SESSION_ID)
-//            ).orElse(null);
-
-            log.info("sessionId={}",sessionId);
-
-            String roomId = chatUtils.getRoodIdFromDestination(destination);
-
-            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
-
-            chatRoomService.enterChatRoomTest(roomId, sessionId, participation.getUser().getEmail(), String.valueOf(participation.getId()));
-
-//            redisPublisher.publish(topic,
-//                    ChatMessageSaveDto.builder()
-//                            .type(ChatMessageSaveDto.MessageType.ENTER)
-//                            .roomId(roomId)
-//                            .userList(chatRoomService.findUser(roomId, sessionId))
-//                            .build()
-//            );
-
 
         }
 
         // 소켓 연결 후 ,SUBSCRIBE 등록
         else if (StompCommand.SUBSCRIBE == accessor.getCommand()) {
 
-//            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
-//
-//            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
-//
-//            log.info("token={}",token);
-//
-//            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
-//            String userId = jwtTokenProvider.getUserId(token);
-//
-//            log.info("userId={}",userId);
-//
-//
-//            String destination = Optional.ofNullable(
-//                    (String) message.getHeaders().get(SIMP_DESTINATION)
-//            ).orElse(INVALID_ROOM_ID);
-//
-//            String sessionId = Optional.ofNullable(
-//                    (String) message.getHeaders().get(SIMP_SESSION_ID)
-//            ).orElse(null);
-//
-//            String roomId = chatUtils.getRoodIdFromDestination(destination);
-//
-//            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
-//
+            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
+
+            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
+
+            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
+
+            String userId = jwtTokenProvider.getUserId(token);
+
+            String destination = Optional.ofNullable(
+                    (String) message.getHeaders().get(SIMP_DESTINATION)
+            ).orElse(INVALID_ROOM_ID);
+
+            String sessionId = Optional.ofNullable(
+                    (String) message.getHeaders().get(SIMP_SESSION_ID)
+            ).orElse(null);
+
+            String roomId = chatUtils.getRoodIdFromDestination(destination);
+
+            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
+
+//            chatRoomService.enterChatRoomTest(roomId, sessionId, participation.getUser().getEmail(), String.valueOf(participation.getId()));
+
+
 //            chatRoomService.enterChatRoom(roomId, sessionId, participation.getUser().getName());
-//
+
 //            redisPublisher.publish(topic,
 //                    ChatMessageSaveDto.builder()
 //                            .type(ChatMessageSaveDto.MessageType.ENTER)
@@ -209,32 +150,32 @@ public class StompHandler implements ChannelInterceptor {
             // TODO: 2023/08/21 세션ID 레디스에 다르게 저장
 
 
-            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
-
-            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
-
-            log.info("token={}",token);
-
-            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
-
-            String userId = jwtTokenProvider.getUserId(token);
-
-            log.info("userId={}",userId);
-
-
-            String destination = Optional.ofNullable(
-                    (String) message.getHeaders().get(SIMP_DESTINATION)
-            ).orElse(INVALID_ROOM_ID);
-
-            String sessionId = Optional.ofNullable(
-                    (String) message.getHeaders().get(SIMP_SESSION_ID)
-            ).orElse(null);
-
-            String roomId = chatUtils.getRoodIdFromDestination(destination);
-
-            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
-
-            chatRoomService.leaveChatRoomTest(String.valueOf(participation.getId()),roomId);
+//            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
+//
+//            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
+//
+//            log.info("token={}",token);
+//
+//            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
+//
+//            String userId = jwtTokenProvider.getUserId(token);
+//
+//            log.info("userId={}",userId);
+//
+//
+//            String destination = Optional.ofNullable(
+//                    (String) message.getHeaders().get(SIMP_DESTINATION)
+//            ).orElse(INVALID_ROOM_ID);
+//
+//            String sessionId = Optional.ofNullable(
+//                    (String) message.getHeaders().get(SIMP_SESSION_ID)
+//            ).orElse(null);
+//
+//            String roomId = chatUtils.getRoodIdFromDestination(destination);
+//
+//            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
+//
+//            chatRoomService.leaveChatRoomTest(String.valueOf(participation.getId()),roomId);
 
 //            redisPublisher.publish(topic,
 //                    ChatMessageSaveDto.builder()
@@ -270,31 +211,31 @@ public class StompHandler implements ChannelInterceptor {
 //            );
 
 
-            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
-
-            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
-
-            log.info("token={}",token);
-
-            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
-            String userId = jwtTokenProvider.getUserId(token);
-
-            log.info("userId={}",userId);
-
-
-            String destination = Optional.ofNullable(
-                    (String) message.getHeaders().get(SIMP_DESTINATION)
-            ).orElse(INVALID_ROOM_ID);
-
-            String sessionId = Optional.ofNullable(
-                    (String) message.getHeaders().get(SIMP_SESSION_ID)
-            ).orElse(null);
-
-            String roomId = chatUtils.getRoodIdFromDestination(destination);
-
-            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
-
-            chatRoomService.disconnectWebsocketTest(String.valueOf(participation.getId()),roomId);
+//            String rawHeader = accessor.getFirstNativeHeader(jwtProperties.getHeader());
+//
+//            String token = jwtTokenProvider.resolveTokenWeb(rawHeader);
+//
+//            log.info("token={}",token);
+//
+//            jwtTokenProvider.validateToken(jwtTokenProvider.resolveTokenWeb(rawHeader));
+//            String userId = jwtTokenProvider.getUserId(token);
+//
+//            log.info("userId={}",userId);
+//
+//
+//            String destination = Optional.ofNullable(
+//                    (String) message.getHeaders().get(SIMP_DESTINATION)
+//            ).orElse(INVALID_ROOM_ID);
+//
+//            String sessionId = Optional.ofNullable(
+//                    (String) message.getHeaders().get(SIMP_SESSION_ID)
+//            ).orElse(null);
+//
+//            String roomId = chatUtils.getRoodIdFromDestination(destination);
+//
+//            Participation participation = participationUtils.findParticipation(Long.valueOf(userId), Long.valueOf(roomId));
+//
+//            chatRoomService.disconnectWebsocketTest(String.valueOf(participation.getId()),roomId);
 
 //            redisPublisher.publish(topic,
 //                    ChatMessageSaveDto.builder()
