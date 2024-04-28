863c1399b3a7d9b51a66f1401ad23ebe5a64b43e
ihun-il <hunil9978@gmail.com>
2024-04-28 14:53:17 +0900

    :sparkles:: 구글 인가 코드 요청 uri 작성 로직 생성

--- src/main/java/ohsoontaxi/backend/domain/credential/service/GoogleOauthStrategy.java
+++ src/main/java/ohsoontaxi/backend/domain/credential/service/GoogleOauthStrategy.java
@@ -1,10 +1,16 @@
 package ohsoontaxi.backend.domain.credential.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ohsoontaxi.backend.domain.credential.presentation.dto.response.OauthTokenInfoDto;
 import ohsoontaxi.backend.global.api.client.GoogleAuthClient;
 import ohsoontaxi.backend.global.api.dto.OIDCPublicKeysResponse;
+import ohsoontaxi.backend.global.api.dto.OauthTokenResponse;
 import ohsoontaxi.backend.global.property.OauthProperties;
 import org.springframework.stereotype.Component;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @AllArgsConstructor
 @Component("GOOGLE")
@@ -14,12 +20,24 @@ public class GoogleOauthStrategy implements OauthStrategy{
     private final GoogleAuthClient googleAuthClient;
     private final OauthOIDCProvider oauthOIDCProvider;
     private static final String ISSUER = "https://accounts.google.com";
+    private static final String QUERY_STRING =
+            "/o/oauth2/v2/auth?response_type=code&client_id=%s&scope=%s&redirect_uri=%s";
+    private static final String scope = "https://www.googleapis.com/auth/userinfo.email";
 
+    public String getOauthLink() {
+        return oauthProperties.getGoogleBaseUrl()
+                + String.format(
+                QUERY_STRING,
+                oauthProperties.getGoogleAppId(),
+                scope,
+                oauthProperties.getGoogleRedirectUrl());
+    }
+
+    @Override
     public OIDCDecodePayload getOIDCDecodePayload(String token) {
         OIDCPublicKeysResponse oidcPublicKeysResponse = googleAuthClient.getGoogleOIDCOpenKeys();
         return oauthOIDCProvider.getPayloadFromIdToken(
                 token, ISSUER, oauthProperties.getGoogleAppId(), oidcPublicKeysResponse);
     }
-
 }
 
